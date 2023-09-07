30ee7f206bb8123f483092a7ea6b5475964356ca
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-07 15:02:52 +0100

    New test class

---
+++ src/test/java/org/example/CarTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+class CarTest {
+
+    @org.junit.jupiter.api.Test
+    void accelerate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void brake() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void honk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayInfo() {
+    }
+}
